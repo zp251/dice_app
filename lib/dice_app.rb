cad254fbc793f7a1b4af1289f9b970dd9be37121
zoe pot <pot.zoe.s@gmail.com>
2018-11-28 12:35:39 +0000

    Roll any number of dice

--- lib/dice_app.rb
+++ lib/dice_app.rb
@@ -1,5 +1,14 @@
 class Dice
-  def roll
-    1 + rand(6)
+  def roll(number)
+    roll_array = []
+    number.times do
+      roll_value = 1 + rand(6)
+      roll_array << roll_value
+    end
+    total = 0
+    roll_array.each do |roll| temporary_total = total + roll
+      total = temporary_total
+    end
+    total
   end
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -4,7 +4,11 @@ RSpec.describe Dice do
 
   it { is_expected.to respond_to(:roll) }
 
-  it "returns random number between 1 and 6" do
-    expect(Dice.new.roll).to be_between(1, 6).inclusive
+#  it "returns random number between 1 and 6" do
+#    expect(Dice.new.roll).to be_between(1, 6).inclusive
+#  end
+
+  it "rolls any number of dice" do
+    expect(Dice.new.roll(3)).to be_between(3, 18).inclusive
   end
 end
